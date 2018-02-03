30da3fa9480f6926fc1f683cea15a730ff38602d
Emma Ramirez <ramirez.emma.g@gmail.com>
2018-02-03 01:42:38 -0700

    init: add App component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export class App extends React.Component<any, any> {
+    constructor(props) {
+        super(props);
+    }
+
+    public render() {
+        return (
+            <div className='app'>
+                Hello
+            </div>
+        );
+    }
+}
---
+++ src/index.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { App } from 'components';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+// @ts-ignore no @types module
+import createHistory from 'history/createBrowserHistory';
+import { Route } from 'react-router';
+// @ts-ignore no @types module
+import { ConnectedRouter } from 'react-router-redux';
+// @ts-ignore no @types module
+import { PersistGate } from 'redux-persist/es/integration/react';
+
+import { store, persistor } from './store';
+
+import 'styles/global.scss';
+
+const mountNode = document.getElementById('root');
+const history = createHistory();
+
+render(
+  <Provider store={store}>
+    <PersistGate
+      loading={<div>Loading...</div>}
+      onBeforeLift={undefined}
+      persistor={persistor}
+    >
+      <ConnectedRouter history={history}>
+        <Route exact path='/' component={App} />
+      </ConnectedRouter>
+    </PersistGate>
+  </Provider>,
+  mountNode
+);
+
+store.subscribe(() => {});
