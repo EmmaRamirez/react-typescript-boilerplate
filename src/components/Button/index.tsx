ef5bd6299845cb47d8abdd1cf925e621e2cc2dfe
Emma Ramirez <ramirez.emma.g@gmail.com>
2018-02-04 22:23:13 -0700

    fix: continue modifying components for react-cosmos

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,16 @@
 import * as React from 'react';
+import styled from 'styled-components';
 
 export class Button extends React.Component {
     public render() {
         return <button>{this.props.children}</button>;
     }
 }
+
+// export const Button = styled.button`
+//     background: #fafafa;
+//     border: 1px solid #ccc;
+//     border-radius: .25rem;
+//     color: #333;
+//     padding: .25rem;
+// `;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,2 @@
 export * from './App';
-export * from './Button';
+export { Button } from './Button';
